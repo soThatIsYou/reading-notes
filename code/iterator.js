0aa3162139a3632f81747d579024ba345f5d65f3
shibo <bo.shi@baidao.com>
2018-06-13 16:46:19 +0800

    iterator

--- code/iterator.js
+++ code/iterator.js
@@ -1,9 +1,27 @@
 let a = new Array();
+a = [1];
 
 console.log(a[Symbol.iterator]);
 
 
+a[Symbol.iterator] = function(){
+  let index = 0;
+  return{next(){
+
+    if(index<a.length){
+        index++
+        return {value:'休想遍历我'};
+    }else {
+      return{value:'快停下来',done:true}
+    }
+
+  }
+}}
+
 let iter = a[Symbol.iterator]();
-
-
 console.log(iter.next());
+console.log(iter.next());
+
+for(let one of a){
+  console.log(one);
+}
